af3fbcc222569dc4bab526e60d4d6d9f5b39639c
Kim JaeHyung <you3192001@icloud.com>
2024-08-22 18:09:03 +0900

    Initial Commit

---
+++ RandomKeyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RandomKeyboard
+//
+//  Created by Jae hyung Kim on 8/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RandomKeyboard/RandomKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomKeyboardApp.swift
+//  RandomKeyboard
+//
+//  Created by Jae hyung Kim on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
