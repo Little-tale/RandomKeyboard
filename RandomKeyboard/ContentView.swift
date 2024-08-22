7ac8ad54f87a13140a38561e26273e41272908bb
Kim JaeHyung <you3192001@icloud.com>
2024-08-22 18:56:19 +0900

    [Feat] 랜덤 키보드 구성해보기

--- RandomKeyboard/ContentView.swift
+++ RandomKeyboard/ContentView.swift
@@ -8,14 +8,114 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State
+    private var numbers: [Int] = Array(0...9).shuffled()
+    
+    @State
+    private var input: String = ""
+    
+    private var grid: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
     var body: some View {
+        contentView()
+    }
+}
+
+extension ContentView {
+    private func contentView() -> some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("핀 번호 입력하세용")
+                .font(.largeTitle)
+            
+            numberShowView()
+            
+            privateShowView()
+            
+            keyboardView()
         }
-        .padding()
+    }
+    
+    private func numberShowView() -> some View {
+        Text(input)
+            .font(.title)
+            .frame(maxWidth: .infinity)
+            .frame(height: 30)
+            .background(Color.gray.opacity(0.2))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding(.horizontal, 60)
+    }
+    
+    @ViewBuilder
+    private func privateShowView() -> some View {
+
+        HStack {
+            ForEach(0...input.count, id: \.self) { current in
+                if current != 0 {
+                    Color.red
+                        .aspectRatio(1, contentMode: .fill)
+                        .frame(width: 10)
+                        .clipShape(Circle())
+                }
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: 30)
+        .background(Color.gray.opacity(0.2))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .padding(.horizontal, 60)
+    }
+    
+    private func keyboardView() -> some View {
+        VStack(alignment: .trailing ,spacing: 8) {
+            LazyVGrid(columns: grid, alignment: .center, spacing: 8) {
+                ForEach(numbers, id: \.self) { number in
+                    if number != numbers.last {
+                        numberView(numberItem: number)
+                    }
+                }
+            }
+            HStack {
+                emptyView()
+                Spacer()
+                numberView(numberItem: numbers.last ?? 0)
+                Spacer()
+                cancelButtonView()
+            }
+            .padding(.horizontal)
+        }
+    }
+    
+    private func cancelButtonView() -> some View {
+        Button {
+            input.removeLast()
+        } label: {
+            Image(systemName: "eraser.fill")
+                .frame(width: 80, height: 80)
+        }
+       
+    }
+    
+    private func emptyView() -> some View {
+        Color.clear
+            .frame(width: 80, height: 80)
+    }
+    
+    private func numberView(numberItem: Int) -> some View {
+        Button {
+            if input.count == 6 {
+                input.removeFirst()
+                input += String(numberItem)
+            } else {
+                input += String(numberItem)
+            }
+        } label: {
+            Text(String(numberItem))
+                .font(.headline).fontWeight(.bold)
+                .frame(width: 80, height: 80)
+                .padding(.all, 10)
+        }
+        .foregroundStyle(.red)
     }
 }
 
